288a012b7bf3a637e5b0b0ff4daf91d9db476f70
Dmitri Pisarev <dimaip@gmail.com>
2016-12-04 00:03:31 +0300

    FEATURE: initial implementation of double opt-in

--- Classes/Psmb/Newsletter/Controller/SubscriptionController.php
+++ Classes/Psmb/Newsletter/Controller/SubscriptionController.php
@@ -6,7 +6,10 @@ use Psmb\Newsletter\Domain\Repository\SubscriberRepository;
 use TYPO3\Flow\Mvc\Controller\ActionController;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Mvc\View\JsonView;
+use TYPO3\Flow\Persistence\PersistenceManagerInterface;
+use TYPO3\Flow\Utility\Algorithms;
 use TYPO3\Fluid\View\TemplateView;
+use TYPO3\SwiftMailer\Message;
 
 class SubscriptionController extends ActionController
 {
@@ -20,9 +23,27 @@ class SubscriptionController extends ActionController
 
     /**
      * @Flow\Inject
+     * @var \TYPO3\Flow\Cache\Frontend\VariableFrontend
+     */
+    protected $tokenCache;
+
+    /**
+     * @Flow\Inject
      * @var SubscriberRepository
      */
     protected $subscriberRepository;
+
+    /**
+     * @Flow\Inject
+     * @var PersistenceManagerInterface
+     */
+    protected $persistenceManager;
+
+    /**
+     * @Flow\Inject
+     * @var \TYPO3\Flow\Mvc\Routing\UriBuilder
+     */
+    protected $uriBuilder;
 
     /**
     * @Flow\InjectConfiguration(path="subscriptions")
@@ -49,9 +70,30 @@ class SubscriptionController extends ActionController
      */
     public function registerAction(Subscriber $newSubscriber)
     {
-        $this->subscriberRepository->add($newSubscriber);
-        $this->redirect('edit', null, null, ['subscriber' => $newSubscriber]);
-        // $this->view->assign('value', array('success' => true));
+        $hash = Algorithms::generateRandomToken(16);
+        $email = $newSubscriber->getEmail();
+        $this->tokenCache->set(
+            $hash,
+            $newSubscriber
+        );
+        $this->sendActivationMail($email, $hash);
+        $this->redirect('index');
+    }
+
+    /**
+     * Create new Subscription based on the hash and delete the hash
+     *
+     * @param string $hash
+     */
+    public function confirmAction($hash)
+    {
+        $newSubscriber = $this->tokenCache->get($hash);
+        if ($newSubscriber) {
+            $this->tokenCache->remove($hash);
+            $this->subscriberRepository->add($newSubscriber);
+            $this->persistenceManager->persistAll();
+        }
+        $this->redirect('index');
     }
 
     /**
@@ -80,4 +122,30 @@ class SubscriptionController extends ActionController
         $this->subscriberRepository->update($subscriber);
         $this->redirect('edit', null, null, ['subscriber' => $subscriber]);
     }
+
+    /**
+     * Sends an activation mail
+     *
+     * @param string $recipientAddress
+     * @param string $hash
+     * @return int
+     */
+    protected function sendActivationMail($recipientAddress, $hash) {
+        $this->uriBuilder->setRequest($this->request);
+        $activationLink = $this->uriBuilder
+            ->setCreateAbsoluteUri(TRUE)
+            ->uriFor(
+                'confirm',
+                ['hash' => $hash],
+                'Subscription',
+                'Psmb.Newsletter'
+            );
+        $mail = new Message();
+        $mail->setFrom(['np-reply@psmb.ru' => 'Your Robot'])
+            ->setTo($recipientAddress)
+            ->setSubject('Activate your account');
+        $mail->setBody($activationLink, 'text/plain');
+        return $mail->send();
+    }
+
 }
